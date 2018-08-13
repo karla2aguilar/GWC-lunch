f001a09204d5a7eff7567c44525ca64362d588fd
karla2aguilar <karla.5225.aguilar@gmail.com>
2018-08-13 09:36:51 -0400

    made containers relative sizes to browser

--- interactive.js
+++ interactive.js
@@ -1,4 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    var sand1 = document.getElementById("mondayImage")
+document.addEventListener('DOMContentLoaded', function (e) {
+    console.log("IT'S LOADEDDDDDDDDDDDDDD");
+
+    var image = document.getElementById('mondayImage');
     
+    var random = true;
+
+    image.addEventListener('click', function (e) {
+        console.log("this one");
+        
+        if (random) {
+            image.src = "file:///C:/Users/GWC12/Documents/whack/sandwichTwo.jpeg";
+            random = false;
+        } else {
+            image.src = "file:///C:/Users/GWC12/Documents/whack/sands.jpeg";
+            random = true;
+        }
+    })
+   
+
+
+
+
+
+
 })
